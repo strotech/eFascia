a38a6fb0413d27e3d36f814486a97e03e0938bd5
DevX Prime <archerearthx@gmail.com>
2021-09-19 20:14:12 +0530

    IC5

--- src/components/panels/views/Tweets.js
+++ src/components/panels/views/Tweets.js
@@ -7,7 +7,7 @@ const Tweets = (props) => {
    return (
       <div className="card text-center overflow-auto">
         <div className="card-body">
-          {tweets && tweets.result.length>0 ? tweets.results.map(tweet=>(
+          {tweets && tweets.result.length>0 ? tweets.result.map(tweet=>(
             <Tweet tweet={tweet} />
           )):null}
         </div>
